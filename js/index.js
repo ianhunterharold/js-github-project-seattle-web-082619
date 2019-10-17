4aedf5439f89460a01ed7c6d197880c4b516427f
ianhunterharold <ianhunterharold@gmail.com>
2019-10-17 13:31:35 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -4,84 +4,143 @@ document.addEventListener('DOMContentLoaded', (event)=>{
 
 const userURL = `https://api.github.com/search/users?q=octocat`;
 
+let searchURL = `https://api.github.com/search/users?q=${formInput}`
+
 function main(){
   listenToFormInput();
-  grabUserData();
-  clickOnName();
+  // grabUserData();
+  // clickOnName();
 }
 
-function listenToFormInput (input){
+function listenToFormInput (){
 
   let gitHubForm = document.getElementById("github-form");
 
   gitHubForm.addEventListener('submit',(event) => {
-  event.preventDefault();
-    console.log("clicked in teh button")
-  let formInput = event.target.search.value;
+    event.preventDefault();
+    
+    let userListId = document.getElementById("user-list");
+   
 
-  fetch(userURL)
-  .then(res => res.json())
-  .then(returnedJson => iterateOverUsersBySearch(returnedJson))
 
+    let formInput = event.target.search.value;
+    // how to i take the input and put it into the search URL
+    let searchURL = `https://api.github.com/search/users?q=${formInput}`
   
+    fetch(searchURL)
+    .then(res => res.json())
+    .then(specificSearchData =>{
+      checkPage(specificSearchData)
+      iterateOverUsersBySearch(specificSearchData)
+    })
   })
-
 }
-// pausing for the time being.
-function iterateOverUsersBySearch(users){
-  for (let i =0; i < users.length; i++){
-    console.log("did we get to this point")
+
+
+function iterateOverUsersBySearch(specificSearchData){
+  for (let i =0; i < specificSearchData['items'].length; i++){
+    let individualAccount = specificSearchData['items'][i]
+    displayUserData(individualAccount); // sending over to function display from the level in which you want to pull data from
   }
 }
 
 
+// function grabUserData(){
+//  //fetch get request on data
+//   fetch(userURL)
+//   .then(res => res.json())
+//   .then(returnedJson => iterateThroughUserData(returnedJson))
+// }
 
-function grabUserData(){
- //fetch get request on data
-  fetch(userURL)
-  .then(res => res.json())
-  .then(returnedJson => iterateThroughUserData(returnedJson))
+// function iterateThroughUserData(userData){
+//   // console.log(userData['items'])
+//   // console.log(userData['items'][i])
+//   for (let i = 0; i < userData['items'].length; i++){
+//     // wasnt catching that i had to iterate over the items array and not the entire datastructure.
+//     // displayUserData(userData['items'][i])
+//     // dont want user data to appear unless it is searched. 
+//   }
+// }
+
+function displayUserData (individualAccount){
+  // console.log("individualAccount",individualAccount)  //showing individual accounts based on the search which is stellar.
+  //get their username, avatar and link to their profil
+
+  let userList = document.getElementById("user-list")
+  // console.log("individualAccount", individualAccount['html_url'])
+  
+  let userName = individualAccount.login;
+  let userNameDiv = document.createElement('div');
+  userNameDiv.innerText = userName;
+
+  let userURL = individualAccount['avatar_url'];
+  let userUrlImg = document.createElement('img');
+  userUrlImg.setAttribute('src',userURL);
+  userUrlImg.setAttribute('alt',userName);
+  // userUrlImg.setAttribute('width',;
+  userUrlImg.innerText = userURL;
+  
+
+  let userHtml = individualAccount['html_url'];
+  let userHtmlDiv = document.createElement('div');
+  userHtmlDiv.innerText = userHtml;
+
+userList.append(userNameDiv,userUrlImg,userHtmlDiv);
+
 }
 
-function iterateThroughUserData(userData){
-  // console.log(userData['items'])
-  // console.log(userData['items'][i])
-  for (let i = 0; i < userData['items'].length; i++){
-    // wasnt catching that i had to iterate over the items array and not the entire datastructure.
-    displayUserData(userData['items'][i])
-  }
-}
 
-function displayUserData (individualUser){
-//get their username, avatar and link to their profil
-// console.log('individual users',individualUsers);
+function checkPage(specificSearchData){
   let userList = document.getElementById("user-list");
-  let userName = individualUser.login;
-  let userId = individualUser.id; 
-
-
-  let userAvatar = individualUser.avatar_url;
-  let userAvatarImage = document.createElement("img");
-
-  let imageUniqueId = document.createElement('id');
-  imageUniqueId.innerText = userId;
-  
-
-  userAvatarImage.setAttribute('src',individualUser.avatar_url);
-  userAvatarImage.setAttribute('alt',individualUser.login);
-  userAvatarImage.innerText = userAvatar;
-  userAvatarImage.append(imageUniqueId);
-
-  let userProfileLink = document.createElement("div");
-  userProfileLink.innerText = individualUser.html_url;
-
-  
-
-  let userLi = document.createElement("li");
-  userLi.innerText = userName;
-  userList.append(userLi,userAvatarImage,userProfileLink);
-
+  while(userList.firstChild){
+    userList.removeChild(userList.firstChild);
+  }
 }
+
+//get their username, avatar and link to their profil
+
+  // let userList = document.getElementById("user-list");
+  // let userName = individualUser[i]['login'];
+  // console.log("userName", userName )
+  // let userId = individualUser['items'].id; 
+
+  // // console.log("individualUser we are at this point",individualUser['items']['login']);
+  // let userAvatar = individualUser['items'].avatar_url;
+  // let userAvatarImage = document.createElement("img");
+
+  // let imageUniqueId = document.createElement('id');
+  // imageUniqueId.innerText = userId;
+
+  // let userProfileLink = document.createElement("div");
+  // userProfileLink.innerText = individualUser['items'].html_url;
+
+  // let userLi = document.createElement("li");
+  // userLi.innerText = userName;
+  // userList.append(userLi,userProfileLink);
+
+
+  // let userList = document.getElementById("user-list");
+  // let userName = individualUser['items'].login;
+  // let userId = individualUser['items'].id; 
+
+  // console.log("individualUser we are at this point",individualUser['items']);
+
+  // let userAvatar = individualUser['items'].avatar_url;
+  // let userAvatarImage = document.createElement("img");
+
+  // let imageUniqueId = document.createElement('id');
+  // imageUniqueId.innerText = userId;
+  
+  
+  // userAvatarImage.setAttribute('src',individualUser.avatar_url);
+
+  // userAvatarImage.setAttribute('alt',individualUser.login);
+  // userAvatarImage.innerText = userAvatar;
+  // userAvatarImage.append(imageUniqueId);
+
+  // pulled out userAvatarImage from above
+
+
 
 function clickOnName(){
 
